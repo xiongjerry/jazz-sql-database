8becce6efdda7eaacd41d5c691a578559e1ade68
Jerry Xiong <jerryxiong@Serenas-MacBook-Air.local>
2021-05-25 17:20:12 -0500

    made changes in server.js. Connected a GET route for artist table and got to connect on client side

--- server/server.js
+++ server/server.js
@@ -28,44 +28,24 @@ pool.on('error', (error) => {
     console.log(error);
 });
 
+// Make POST and GET route for 'artist'
+
+app.get('/artist', (req, res) => {
+    //connect the route from /artist to SQL database
+    const queryText = `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`
+    pool.query(queryText)
+    .then( (artistList) =>{
+        console.log('lists of artist', artistList.rows);
+        res.send(artistList.rows);
+    }).catch( (err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+})
 
 
-// // TODO - Replace static content with a database tables
-// const artistList = [ 
-//     {
-//         name: 'Ella Fitzgerald',
-//         birthdate: '04-25-1917'
-//     },
-//     {
-//         name: 'Dave Brubeck',
-//         birthdate: '12-06-1920'
-//     },       
-//     {
-//         name: 'Miles Davis',
-//         birthdate: '05-26-1926'
-//     },
-//     {
-//         name: 'Esperanza Spalding',
-//         birthdate: '10-18-1984'
-//     },
-// ]
-// const songList = [
-//     {
-//         title: 'Take Five',
-//         length: '5:24',
-//         released: '1959-09-29'
-//     },
-//     {
-//         title: 'So What',
-//         length: '9:22',
-//         released: '1959-08-17'
-//     },
-//     {
-//         title: 'Black Gold',
-//         length: '5:17',
-//         released: '2012-02-01'
-//     }
-// ];
+
+// Make POST and GET route for 'song'
 
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);

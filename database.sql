bd5734601eb5ed20125597dbb9f527fb9dec4066
Jerry Xiong <jerryxiong@Serenas-MacBook-Air.local>
2021-05-25 16:45:04 -0500

    made a database postico. Inside postico, made two tables (song and artist) added name and birthday and name to artist table. Added title, length, release date to song table. Inserted given data on server.js to the SQL query. Updated database.sql with the inserts and created tables

--- database.sql
+++ database.sql
@@ -1,5 +1,37 @@
-CREATE TABLE "artists" (
-    "id" SERIAL PRIMARY KEY,
-    "artist_name" varchar(80) not null,
-    "year_born" date
+CREATE TABLE "artist" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80),
+	"birthdate" DATE
 );
+	
+	
+CREATE TABLE "song" (
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(255),
+	"length" VARCHAR(10),
+	"released" DATE
+);
+
+-- ADDING SONGS AND ARTISTS
+
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Ella Fitzgerald', '04/25/1917');
+
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Dave Brubeck', '12/06/1920');
+ 
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Miles Davis', '05/26/1926');
+
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Esperanza Spalding', '10/18/1984');
+
+
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('Take Five', '5:24', '1920/09/29');
+
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('So What', '9:22', '1959/08/17');
+
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('Black Gold', '5:17', '2012/02/01');

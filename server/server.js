7627163f3fc8481ca61174fc18f82c035ec73bce
Jerry Xiong <jerryxiong@Serenas-MacBook-Air.local>
2021-05-25 16:58:26 -0500

    installed npm pg. made some initalizing with pg codes on server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pg = require('pg');
 
 const app = express();
 const PORT = 5000;
@@ -10,43 +11,61 @@ app.use(express.static('server/public'));
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
+// pg configuration -- LOCAL ONLY
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'jazzy_sql',
+    host: 'Localhost',
+    port: 5432,
+})
 
-// TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
+// check to see connection
+pool.on('connect', () =>{
+    console.log('CONNECTED TO POSTGRES');
+});
+// checking if there are errors
+pool.on('error', (error) => {
+    console.log(error);
+});
+
+
+
+// // TODO - Replace static content with a database tables
+// const artistList = [ 
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },       
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
+// const songList = [
+//     {
+//         title: 'Take Five',
+//         length: '5:24',
+//         released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         released: '1959-08-17'
+//     },
+//     {
+//         title: 'Black Gold',
+//         length: '5:17',
+//         released: '2012-02-01'
+//     }
+// ];
 
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);
